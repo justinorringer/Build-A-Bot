f4157946dee996d05824c97f83f2b8cf59514ab1
CharlieLomax <cwlomax@ncsu.edu>
2022-03-03 20:25:47 -0500

    Fixed bug preventing jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,6 @@ namespace BuildABot
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerMovement : CharacterMovement
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
 
         // Update is called once per frame
         void Update()
