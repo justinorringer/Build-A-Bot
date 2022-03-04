3c5b809d0ac0eac6ae3d9816b160d957df155e7b
cwlomax <cwlomax@ncsu.edu>
2022-03-04 13:55:31 -0500

    Fixed collision issues

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -16,14 +16,21 @@ namespace BuildABot
         // This object's rigidbody
         private Rigidbody2D _rigidbody;
 
-        private bool _jumping = false;
-
         // Number of double jumps the player can do
         private int _maxJumps = 2;
         // Whether the player has already spent their double jump
         private int _numJumps;
         // Whether the player has unlocked the ability to double jump
         private bool _doubleJumpEnabled = true;
+
+        // Whether the character is moving left this frame
+        private bool _movingLeft = false;
+        // Whether the character is moving right this frame
+        private bool _movingRight = false;
+        // Whether the character is moving up this frame
+        private bool _movingUp = false;
+        // Whether the character is moving down this frame
+        private bool _movingDown = false;
 
         // Whether the player is currently touching the ground
         private bool _isGrounded = false;
@@ -40,36 +47,73 @@ namespace BuildABot
             _yDist = GetComponent<Collider2D>().bounds.extents.y + 0.1f;
         }
 
+        public void FixedUpdate()
+        {
+            // Move based on the set movement flags
+
+            if(_movingLeft)
+            {
+                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+                _movingLeft = false;
+            }
+
+            if(_movingRight)
+            {
+                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                _movingRight = false;
+            }
+
+            if (_movingUp)
+            {
+                transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+                _movingUp = false;
+            }
+
+            if (_movingDown)
+            {
+                transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+                _movingDown = false;
+            }
+        }
+
         void OnCollisionEnter2D(Collision2D collision)
         {
+            // When a character enters a collision, check whether it has landed on the ground
             checkGrounded();
         }
 
+        // When a character exits a collision, check whether it has left the ground
         void OnCollisionExit2D(Collision2D collision)
         {
+            // When a character exits a collision, check whether it has left the ground
             checkGrounded();
         }
 
+        // Translate the character left based on their move speed
         public void MoveLeft()
         {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            _movingLeft = true;
         }
 
+        // Translate the character right based on their move speed
         public void MoveRight()
         {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            _movingRight = true;
         }
 
+        // Translate the character up based on their move speed
         public void MoveUp()
         {
-            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
-        }
-        
-        public void MoveDown()
-        {
-            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+            _movingUp = true;
         }
 
+        // Translate the character down based on their move speed
+        public void MoveDown()
+        {
+            _movingDown = true;
+        }
+
+        // Check if a jump is allowed, and if so, jump
         public void StartJump()
         {
             // If the player is on the ground, a jump is permitted
@@ -77,15 +121,16 @@ namespace BuildABot
             {
                 _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
+            // If the player has double jumps remaining, a jump is permitted
             else if (_doubleJumpEnabled && _numJumps > 0)
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
                 _rigidbody.AddForce(Vector2.up * doubleJumpForce, ForceMode2D.Impulse);
                 _numJumps--;
             }
-            //_jumping = true;
         }
 
+        // Checks if the player is grounded, and updates isGrounded value accordingly
         void checkGrounded()
         {
             _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, _yDist);
