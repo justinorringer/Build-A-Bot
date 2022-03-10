e75c609c9b692b898d207eca31d05fead6d18e9b
CharlieLomax <cwlomax@ncsu.edu>
2022-03-10 14:30:27 -0500

    Added some state to Attack script

--- Assets/BuildABot/Scripts/Attack.cs
+++ Assets/BuildABot/Scripts/Attack.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    // Width of the attack's hitbox
+    [SerializeField] private float hitboxWidth;
+    // Height of the attack's hitbox
+    [SerializeField] private float hitboxHeight;
+
+    // Prefab used to create attack hitbox
+    [SerializeField] private GameObject attackPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
