7d1ceb420748abf7279dbd71baef8151cdc732e7
cwlomax <cwlomax@ncsu.edu>
2022-04-14 18:01:57 -0400

    Fixed animation stutter

--- Assets/BuildABot/Scripts/Character/PlayerMovement.cs
+++ Assets/BuildABot/Scripts/Character/PlayerMovement.cs
@@ -37,9 +37,9 @@ namespace BuildABot
             if (Animator != null && Animator.runtimeAnimatorController != null)
             {
                 // Tell animator if player is running
-                Animator.SetBool(_runningBoolHash, InMotion && IsGrounded);
+                Animator.SetBool(_runningBoolHash, direction.x != 0.0f && IsGrounded);
                 // Tell animator if player is idle
-                Animator.SetBool(_idleBoolHash, !InMotion);
+                Animator.SetBool(_idleBoolHash, Velocity == Vector2.zero);
             }
         }
 
