d8a499df18514a068da2ce799070818cfa4456a6
Zachary Harris <zmharri2@ncsu.edu>
2022-04-30 02:54:38 -0400

    Removed offset from BIPY generate.

--- Assets/BuildABot/Scripts/Generation/BipyGenerate.cs
+++ Assets/BuildABot/Scripts/Generation/BipyGenerate.cs
@@ -9,8 +9,6 @@ namespace BuildABot {
 
         private LevelGenerator levelGenerator;
 
-        [SerializeField] private int[] pos = new int[2] { 10, 10 };
-
         void Start() {
             levelGenerator = GameObject.Find("Generator").GetComponent<LevelGenerator>();
         }
@@ -18,7 +16,7 @@ namespace BuildABot {
         void Update() {
             if (!levelGenerator.generate) {
                 Vector3 position = transform.position;
-                Vector3 p = new Vector3(position.x + pos[0], position.y + pos[1], 0);
+                Vector3 p = new Vector3(position.x, position.y, 0);
                 Player player = GameManager.GetPlayer();
                 if (player == null)
                 {
