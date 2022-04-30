d3b1b71c749e7637d0ec06b8b9a057cf0ace2c64
Zachary Harris <zmharri2@ncsu.edu>
2022-04-30 02:53:07 -0400

    Added restart level to the pause menu. Added gamestage based loot for the merchant.

--- Assets/BuildABot/Scripts/Generation/BipyGenerate.cs
+++ Assets/BuildABot/Scripts/Generation/BipyGenerate.cs
@@ -18,15 +18,14 @@ namespace BuildABot {
         void Update() {
             if (!levelGenerator.generate) {
                 Vector3 position = transform.position;
+                Vector3 p = new Vector3(position.x + pos[0], position.y + pos[1], 0);
                 Player player = GameManager.GetPlayer();
                 if (player == null)
                 {
-                    Vector3 p = new Vector3(position.x + pos[0], position.y + pos[1], 0);
                     Instantiate(Bipy, p, Quaternion.identity);
                 }
                 else
                 {
-                    Vector3 p = new Vector3(position.x + pos[0], position.y + pos[1], 0);
                     player.gameObject.transform.position = p;
                     player.CharacterMovement.ClearMovement();
                 }
