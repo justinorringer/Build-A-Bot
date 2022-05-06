199177b106e040c4a2582a384ef76f475eeaaf51
justinorringer <jmorring@ncsu.edu>
2022-05-05 23:02:15 -0400

    Uploaded Interactable Elevator script again

--- Assets/BuildABot/Scripts/Level/Elevator.cs
+++ Assets/BuildABot/Scripts/Level/Elevator.cs
@@ -5,25 +5,35 @@ using Random = UnityEngine.Random;
 
 namespace BuildABot
 {
-    public class Elevator : MonoBehaviour
+    public class Elevator : InteractableObject
     {
 
         [SerializeField] private string sceneToLoad;
-        
-        private void OnTriggerEnter2D(Collider2D other)
+
+        /** The current gamer. */
+        public Player Gamer { get; private set; }
+
+        protected override void OnInteract(InteractionController instigator)
         {
-            Player player = other.gameObject.GetComponent<Player>();
-            if (player != null)
-            {
-                GameManager.GameState.CompletedLevelCount++;
-                if (GameManager.GameState.CompletedLevelCount == 3) GameManager.GameState.GameStage++;
-                else if (GameManager.GameState.CompletedLevelCount == 5) GameManager.GameState.GameStage++;
-                else if (GameManager.GameState.CompletedLevelCount == 7) GameManager.GameState.GameStage++;
-                GameManager.GameState.NextLevelType = GameManager.GameState.GameStage >= 3
-                    ? Random.Range(0, 3)
-                    : GameManager.GameState.GameStage;
-                GameManager.OpenLevel(sceneToLoad);
-            }
+            Debug.LogFormat("Elevator: {0}", sceneToLoad);
+            Gamer = instigator.Player;
+            GoingUp();
+            base.OnInteract(instigator);
         }
+
+        private void GoingUp()
+        {
+            Debug.LogFormat("In ButtonPress");
+
+            GameManager.GameState.CompletedLevelCount++;
+            if (GameManager.GameState.CompletedLevelCount == 3) GameManager.GameState.GameStage++;
+            else if (GameManager.GameState.CompletedLevelCount == 5) GameManager.GameState.GameStage++;
+            else if (GameManager.GameState.CompletedLevelCount == 7) GameManager.GameState.GameStage++;
+            GameManager.GameState.NextLevelType = GameManager.GameState.GameStage >= 3
+                ? Random.Range(0, 3)
+                : GameManager.GameState.GameStage;
+            
+            GameManager.OpenLevel(sceneToLoad);
+       }
     }
 }
